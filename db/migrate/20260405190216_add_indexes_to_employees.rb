cef5c1ed2f96f45c5d090526c93c1067ae298c1d
Ayushi Ghorpade <79832848+Ayushi6896@users.noreply.github.com>
2026-04-06 00:49:08 +0530

    Seed 10,000 employees efficiently

--- db/migrate/20260405190216_add_indexes_to_employees.rb
+++ db/migrate/20260405190216_add_indexes_to_employees.rb
@@ -2,6 +2,6 @@ class AddIndexesToEmployees < ActiveRecord::Migration[8.0]
   def change
     add_index :employees, :country
     add_index :employees, :job_title
-    add_index :employees, [:country, :job_title]
+    add_index :employees, [ :country, :job_title ]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,21 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+first_names = File.read(Rails.root.join("db/content/first_names.txt")).split("\n")
+last_names  = File.read(Rails.root.join("db/content/last_names.txt")).split("\n")
+job_titles  = [ "Engineer", "Manager", "Designer", "QA", "HR" ]
+countries   = [ "India", "USA", "UK", "Germany", "France" ]
+
+raise "first_names.txt is empty" if first_names.empty?
+raise "last_names.txt is empty"  if last_names.empty?
+
+employees_data = []
+
+10_000.times do
+  employees_data << {
+    full_name: "#{first_names.sample} #{last_names.sample}",
+    job_title: job_titles.sample,
+    country: countries.sample,
+    salary: rand(40_000..200_000)
+  }
+end
+
+Employee.insert_all(employees_data)
+puts "Seeded 10,000 employees!"
