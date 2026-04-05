8513a9c635dffe67d3431d412be87bdef14e3a55
Ayushi Ghorpade <79832848+Ayushi6896@users.noreply.github.com>
2026-04-05 23:11:58 +0530

    Add salary and name validations to satisfy tests

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,5 @@
 class Employee < ApplicationRecord
   validates :full_name, :job_title, :country, :salary, presence: true
+  validates :salary, numericality: { greater_than: 0 }
+  validates :full_name, length: { minimum: 2 }
 end
