045fab863d2982c503f1ba66af83d38c529fafc3
Ayushi Ghorpade <79832848+Ayushi6896@users.noreply.github.com>
2026-04-05 23:03:17 +0530

    Add Employee model with basic validations to satisfy tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 0) do
+ActiveRecord::Schema[8.0].define(version: 2026_04_05_172507) do
+  create_table "employees", force: :cascade do |t|
+    t.string "full_name"
+    t.string "job_title"
+    t.string "country"
+    t.integer "salary"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
