7f033dc00287de8aae5743df7b512530fb0f0721
Ayushi Ghorpade <79832848+Ayushi6896@users.noreply.github.com>
2026-04-06 22:39:45 +0530

    Enable CORS for frontend integration

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -5,12 +5,16 @@
 
 # Read more: https://github.com/cyu/rack-cors
 
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins "example.com"
-#
-#     resource "*",
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allowed_origins = ENV.fetch("CORS_ALLOWED_ORIGINS", "http://localhost:5173")
+                       .split(",").map { |origin| origin.strip }
+
+  allow do
+    origins allowed_origins
+
+    resource "*",
+             headers: :any,
+             methods: [ :get, :post, :put, :patch, :delete, :options, :head ],
+             credentials: true
+  end
+end
