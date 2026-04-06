42424cbd681932c9dcb6fb1f5d285af1795208d8
Ayushi Ghorpade <79832848+Ayushi6896@users.noreply.github.com>
2026-04-06 22:00:27 +0530

    Add failing tests for employees pagination

--- test/controllers/employees_controller_test.rb
+++ test/controllers/employees_controller_test.rb
@@ -95,4 +95,41 @@ class EmployeesControllerTest < ActionDispatch::IntegrationTest
     expected_msg = I18n.t("error.required", field: I18n.t("field.country"))
     assert_equal expected_msg, json["error"]
   end
+
+  test "should paginate employees list with default values" do
+    30.times { |i| create_employee(full_name: "Employee #{i}") }
+    get "/employees"
+    assert_response :success
+    json = JSON.parse(response.body)
+
+    assert json["employees"].is_a?(Array)
+    assert_equal 30, json["employees"].length
+    assert_equal 30, json["pagination"]["total_count"]
+    assert_equal 1, json["pagination"]["current_page"]
+    assert_equal 1, json["pagination"]["total_pages"]
+    assert_equal 50, json["pagination"]["per_page"]
+  end
+
+  test "should paginate employees with custom per_page" do
+    55.times { |i| create_employee(full_name: "Employee #{i}") }
+    get "/employees", params: { page: 1, per_page: 20 }
+    assert_response :success
+    json = JSON.parse(response.body)
+
+    assert_equal 20, json["employees"].length
+    assert_equal 55, json["pagination"]["total_count"]
+    assert_equal 1, json["pagination"]["current_page"]
+    assert_equal 3, json["pagination"]["total_pages"]
+    assert_equal 20, json["pagination"]["per_page"]
+  end
+
+  test "should return second page of employees" do
+    55.times { |i| create_employee(full_name: "Employee #{i}") }
+    get "/employees", params: { page: 2, per_page: 50 }
+    assert_response :success
+    json = JSON.parse(response.body)
+
+    assert_equal 5, json["employees"].length
+    assert_equal 2, json["pagination"]["current_page"]
+  end
 end
